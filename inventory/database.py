d7e1be0dc38749a832fafe92babd52622cde987e
mercynjambi <njambim695@gmail.com>
2024-07-31 22:59:56 +0300

    updated database

--- inventory/database.py
+++ inventory/database.py
@@ -2,20 +2,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Connect to the SQLite database
-DATABASE_URL = ('sqloite:///./inventory.db')
 
-# create Engine
+DATABASE_URL = ('sqlite:///./inventory.db')
+
+
 engine = create_engine(DATABASE_URL)
 
 # Session Local class
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Base Class
+
 Base = declarative_base()
 
-def init_db(): # ensure db schema is setup by creating the necessary tables based on model definitions
-    import inventory.models #import models
-    Base.metadata.create_all(bind=engine) # create tables
+def init_db(): 
+    import inventory.models 
+    Base.metadata.create_all(bind=engine) 
 
 
