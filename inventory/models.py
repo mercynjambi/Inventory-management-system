2c4f683b8de2220ad27ad2862f67353c660eeb05
mercynjambi <njambim695@gmail.com>
2024-07-31 22:50:58 +0300

    chnged price to float

--- inventory/models.py
+++ inventory/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, Float, String, ForeignKey
 from inventory.database import Base
 from sqlalchemy.orm import relationship
 
@@ -6,17 +6,17 @@ class Category(Base):
     __tablename__ = 'categories'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True, nulable=True)
+    name = Column(String, unique=True, nullable=False)
     items = relationship('item', back_populates='category')
 
 class Item(Base):
     __tablename__ = 'items'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, nulable=False)
+    name = Column(String, nullable=False)
     description = Column(String)
     quantity = Column(Integer, default=0)
-    price = Column(Integer)
+    price = Column(Float)
     category_id = Column(Integer, ForeignKey('categories.id'))
     category = relationship('Category', back_populates='items')
 
