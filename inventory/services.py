7293ae42b96e0cf44be142b900dbe969fd191c08
mercynjambi <njambim695@gmail.com>
2024-08-01 20:01:32 +0300

    updated customer, supllier and order services

--- inventory/services.py
+++ inventory/services.py
@@ -1,6 +1,6 @@
 
 from inventory.database import SessionLocal
-from inventory.models import Category, Item
+from inventory.models import Category, Item, Order, Supplier, Customer 
 
 def get_session():
     return SessionLocal()  
@@ -62,5 +62,77 @@ def find_category_by_name(name):
     category = session.query(Category).filter(Category.name == name).first()
     session.close()
     return category
+
+def add_supplier(name, contact_info):
+    session = get_session()
+    supplier = Supplier(name=name, contact_info=contact_info)
+    session.add(supplier)
+    session.commit()
+    session.close()
+
+def get_suppliers():
+    session = get_session()
+    suppliers = session.query(Supplier).all()
+    session.close()
+    return suppliers
+
+def find_supplier_by_id(supplier_id):
+    session = get_session()
+    supplier = session.query(Supplier).filter(Supplier.id == supplier_id).first()
+    session.close()
+    return supplier
+
+def find_supplier_by_name(name):
+    session = get_session()
+    supplier = session.query(Supplier).filter(Supplier.name == name).first()
+    session.close()
+    return supplier
+
+# Customer Services
+def add_customer(name, email):
+    session = get_session()
+    customer = Customer(name=name, email=email)
+    session.add(customer)
+    session.commit()
+    session.close()
+
+def get_customers():
+    session = get_session()
+    customers = session.query(Customer).all()
+    session.close()
+    return customers
+
+def find_customer_by_id(customer_id):
+    session = get_session()
+    customer = session.query(Customer).filter(Customer.id == customer_id).first()
+    session.close()
+    return customer
+
+def find_customer_by_email(email):
+    session = get_session()
+    customer = session.query(Customer).filter(Customer.email == email).first()
+    session.close()
+    return customer
+
+# Order Services
+def add_order(item_id, quantity, customer_id):
+    session = get_session()
+    order = Order(item_id=item_id, quantity=quantity, customer_id=customer_id)
+    session.add(order)
+    session.commit()
+    session.close()
+
+def get_orders():
+    session = get_session()
+    orders = session.query(Order).all()
+    session.close()
+    return orders
+
+def find_order_by_id(order_id):
+    session = get_session()
+    order = session.query(Order).filter(Order.id == order_id).first()
+    session.close()
+    return order
+
 
 
