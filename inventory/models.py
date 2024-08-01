5dd3e59566572f87b56f789ef7a856d9e850c09c
mercynjambi <njambim695@gmail.com>
2024-08-01 19:42:48 +0300

    created Order, supplier and Customer Class

--- inventory/models.py
+++ inventory/models.py
@@ -1,6 +1,8 @@
-from sqlalchemy import Column, Integer, Float, String, ForeignKey
-from inventory.database import Base
+
+from sqlalchemy import Column, Integer, Float, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
+from inventory.database import Base
+from datetime import datetime
 
 class Category(Base):
     __tablename__ = 'categories'
@@ -19,12 +21,34 @@ class Item(Base):
     price = Column(Float)
     category_id = Column(Integer, ForeignKey('categories.id'))
     category = relationship('Category', back_populates='items')
+    supplier_id = Column(Integer, ForeignKey('suppliers.id'))
+    supplier = relationship('Supplier', back_populates='items')
+    orders = relationship('Order', back_populates='item')
 
+class Supplier(Base):
+    __tablename__ = 'suppliers'
 
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True, nullable=False)
+    contact_info = Column(String)
+    items = relationship('Item', back_populates='supplier')
 
+class Customer(Base):
+    __tablename__ = 'customers'
 
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True)
+    orders = relationship('Order', back_populates='customer')
 
+class Order(Base):
+    __tablename__ = 'orders'
 
+    id = Column(Integer, primary_key=True)
+    item_id = Column(Integer, ForeignKey('items.id'))
+    customer_id = Column(Integer, ForeignKey('customers.id'))
+    quantity = Column(Integer, nullable=False)
+    order_date = Column(DateTime, default=datetime.utcnow)
 
-    
-
+    item = relationship('Item', back_populates='orders')
+    customer = relationship('Customer', back_populates='orders')
